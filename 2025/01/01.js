e02cdcc1fc5b2a5a292f0cb95a823bd3d933cd1d
Bryan <bryanfinesw@gmail.com>
2025-12-02 21:43:20 -0500

    Part 1 & 2 Completed

--- 2025/01/01.js
+++ 2025/01/01.js
@@ -1,5 +1,56 @@
 import fs from "fs/promises";
 
-const input = await fs.readFile('./sample.txt', 'utf8');
+const input = await fs.readFile('./input.txt', 'utf8');
+const data = input.trim().split("\n");
 
-console.log(input);
+function partOne(rotations) {
+    const START = 50;
+    const MAX = 100;
+    let current = START;
+    let counter = 0;
+
+    rotations.forEach((rotation) => {
+        const dir = rotation.slice(0, 1);
+        const value = Number(rotation.slice(1));
+
+        if (dir === "L") {
+            current = (current - value + MAX) % MAX;
+        } else if (dir === "R") {
+            current = (current + value) % MAX;
+        }
+
+        if (current === 0) {
+            counter += 1;
+        }
+    });
+
+    return counter;
+};
+
+console.log(partOne(data));
+
+function partTwo(rotations) {
+    const START = 50;
+    const MAX = 100;
+    let current = START;
+    let counter = 0;
+
+    rotations.forEach((rotation) => {
+        const dir = rotation.slice(0, 1);
+        const value = Number(rotation.slice(1));
+
+        for (let i = 0; i < value; i++) {
+            current = dir === "L"
+                ? (current - 1 + MAX) % MAX
+                : (current + 1) % MAX;
+
+            if (current === 0) {
+                counter += 1;
+            }
+        }
+    })
+
+    return counter;
+};
+
+console.log(partTwo(data));
